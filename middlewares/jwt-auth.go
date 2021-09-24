5d4e422a5e4bf7e6dac2f09fb333671a7838132a
a180024 <a180024@e.ntu.edu.sg>
2021-09-25 01:58:48 +0800

    update jwt middleware

--- middlewares/jwt-auth.go
+++ middlewares/jwt-auth.go
@@ -19,8 +19,6 @@ func AuthorizeJWT() gin.HandlerFunc {
 
 		if token.Valid {
 			claims := token.Claims.(jwt.MapClaims)
-			log.Println("Claims[Name]: ", claims["name"])
-			log.Println("Claims[Admin]: ", claims["admin"])
 			log.Println("Claims[Issuer]: ", claims["iss"])
 			log.Println("Claims[IssuedAt]: ", claims["iat"])
 			log.Println("Claims[ExpiresAt]: ", claims["exp"])
